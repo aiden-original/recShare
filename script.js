2784bdb8da950d980043c09087affaabb2fd356e
aiden-original <mobereolanedia@gmail.com>
2025-09-05 19:30:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // hosting was chat gpt so no questions
 
+const { createClient } = supabase;
 // Initialize Supabase
   const supabaseUrl = 'https://khcmowenbrddwujsxooi.supabase.co';
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoY21vd2VuYnJkZHd1anN4b29pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MjUzMTQsImV4cCI6MjA3MTIwMTMxNH0._ybMDfD1y7JldmM-rOjJu97vJ2IBPgd5zVjYtSwY3Xg';
