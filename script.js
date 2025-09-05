ecd968aa924d61c0994cd500c28586615bdefebe
aiden-original <mobereolanedia@gmail.com>
2025-09-05 20:09:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@
 // Initialize Supabase
   const supabaseUrl = 'https://khcmowenbrddwujsxooi.supabase.co';
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoY21vd2VuYnJkZHd1anN4b29pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MjUzMTQsImV4cCI6MjA3MTIwMTMxNH0._ybMDfD1y7JldmM-rOjJu97vJ2IBPgd5zVjYtSwY3Xg';
-  const supabase = createClient(supabaseUrl, supabaseKey);
-
+  const Supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 
 
