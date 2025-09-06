f5dce01098ef932e905c8b589a7c4599a376182b
aiden-original <mobereolanedia@gmail.com>
2025-09-06 09:13:40 +0100

    Update scrisssssssssssssssspt.js

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ console.log(supabase);
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoY21vd2VuYnJkZHd1anN4b29pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MjUzMTQsImV4cCI6MjA3MTIwMTMxNH0._ybMDfD1y7JldmM-rOjJu97vJ2IBPgd5zVjYtSwY3Xg';
   const Supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
-let { data, error } = await supabase
-  .from("recipes")
-  .select("*");
+async function testRecipes() {
+  let { data, error } = await Supabase
+    .from("recipes")
+    .select("*");
 
-console.log("ALL recipes:", data, error);
+  console.log("ALL recipes:", data, error);
+}
+
+testRecipes();
