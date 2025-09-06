14ae373c429ab80529c915268de01be627dc210c
aiden-original <mobereolanedia@gmail.com>
2025-09-06 10:54:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ console.log(supabase);
   const supabaseUrl = 'https://khcmowenbrddwujsxooi.supabase.co';
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoY21vd2VuYnJkZHd1anN4b29pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MjUzMTQsImV4cCI6MjA3MTIwMTMxNH0._ybMDfD1y7JldmM-rOjJu97vJ2IBPgd5zVjYtSwY3Xg';
   const Supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const urlsList = ["https://img.freepik.com/premium-vector/salad-bowl-clipart-cartoon-style-vector-illustration_761413-15628.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA_f36nJiE8EgAN6t3EwMmwVl4Tp8oJTcPjQ&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwF5BpIjuqZ8V8c4D4WGFkHd11LVKC2ZZm3w&s",
+]
 
 let counter = 1
 // 1️⃣ Fetch recipe from Supabase
